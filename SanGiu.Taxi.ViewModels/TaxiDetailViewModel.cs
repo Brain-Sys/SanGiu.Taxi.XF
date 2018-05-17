e2ff9f1417a0414d2bd66db3f9b3a8e860941411
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-05-17 15:40:17 +0200

    -

--- SanGiu.Taxi.ViewModels/TaxiDetailViewModel.cs
+++ SanGiu.Taxi.ViewModels/TaxiDetailViewModel.cs
@@ -4,6 +4,8 @@ using Plugin.Geolocator.Abstractions;
 using SanGiu.Taxi.ViewModels.VM;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -80,18 +82,34 @@ namespace SanGiu.Taxi.ViewModels
             if (CrossGeolocator.Current.IsGeolocationAvailable)
             {
 #if DEBUG
-                await Task.Delay(10000);
+                await Task.Delay(2000);
 #endif
                 tokenSource = new CancellationTokenSource();
                 Position position = await CrossGeolocator.Current
                     .GetPositionAsync(TimeSpan.FromMinutes(1), tokenSource.Token);
-
                 this.Latitude = position.Latitude;
                 this.Longitude = position.Longitude;
+
+                await test();
+                //var addresses = await CrossGeolocator.Current.GetAddressesForPositionAsync(position, "");
+                //foreach (var item in addresses)
+                //{
+                //    Debug.WriteLine(item.ToString());
+                //}
             }
 
             this.IsBusy = false;
             this.GetPositionCommand.RaiseCanExecuteChanged();
+        }
+
+        private async Task test()
+        {
+            string stringLatitude = latitude.ToString().Replace(',', '.');
+            string stringLongitude = longitude.ToString().Replace(',', '.');
+            string url = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?latlng={0},{1}&sensor=false", stringLatitude, stringLongitude);
+
+            HttpClient client = new HttpClient();
+            string xml = await client.GetStringAsync(url);
         }
     }
 }
