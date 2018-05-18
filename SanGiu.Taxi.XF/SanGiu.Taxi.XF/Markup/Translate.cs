d1d37e8408addaa8010752280a04fff8f3b1e073
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-05-18 12:08:49 +0200

    -

---
+++ SanGiu.Taxi.XF/SanGiu.Taxi.XF/Markup/Translate.cs
@@ -0,0 +1,24 @@
+﻿using SanGiu.Taxi.Translations;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms.Xaml;
+
+namespace SanGiu.Taxi.XF.Markup
+{
+    class Translate : IMarkupExtension
+    {
+        public string Language { get; set; }
+        public string Key { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var manager = Vocabulary.ResourceManager;
+            var c = CultureInfo.CreateSpecificCulture(this.Language);
+            string result = manager.GetString(this.Key, c);
+
+            return result;
+        }
+    }
+}
